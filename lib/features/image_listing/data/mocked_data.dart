121a57d0295dbcb4d3deda635bfe6df07ccf00fc
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-02-24 02:15:48 -0300

    feat: create json for mocked data

---
+++ lib/features/image_listing/data/mocked_data.dart
@@ -0,0 +1,146 @@
+const mockImagesListing = [
+  {
+    "category": "Nature",
+    "title": "Flores do Campo",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2023/02/14/18/55/flowers-7790227_1280.jpg"
+  },
+  {
+    "category": "Animals",
+    "title": "Sono do Outuno",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/03/27/22/22/fox-1284512_1280.jpg"
+  },
+  {
+    "category": "Animals",
+    "title": "Asas do Gigante",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_1280.jpg"
+  },
+  {
+    "category": "Animals",
+    "title": "Olhar Congelante",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_1280.jpg"
+  },
+  {
+    "category": "Animals",
+    "title": "Fofura Agressiva",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2014/10/01/10/44/animal-468228_1280.jpg"
+  },
+  {
+    "category": "Animals",
+    "title": "A Corrida Final",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2014/08/29/03/02/horses-430441_1280.jpg"
+  },
+  {
+    "category": "Animals",
+    "title": "Olhar De Mel",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/07/15/15/55/dachshund-1519374_1280.jpg"
+  },
+  {
+    "category": "Food",
+    "title": "Saúde Em Primeiro Lugar",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg"
+  },
+  {
+    "category": "Food",
+    "title": "Especiarias",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2014/06/16/23/10/spices-370114_1280.jpg"
+  },
+  {
+    "category": "Food",
+    "title": "Pimentão Em Queda Livre",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/04/02/04/14/bell-peppers-1302126_1280.jpg"
+  },
+  {
+    "category": "Food",
+    "title": "O Café Dos Campeões",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2017/01/16/17/45/pancake-1984716_1280.jpg"
+  },
+  {
+    "category": "Food",
+    "title": "Mar De Café",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/04/12/11/19/coffee-1324126_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "A Tempestade",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "Calmaria",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "Lago Espelhado",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "Eucaliptos",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2018/08/21/23/29/forest-3622519_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "O Alcance De Uma Gota",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2016/11/06/05/36/lake-1802337_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "Penguin Colony",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2023/02/06/01/14/superb-fairywren-7770904_1280.jpg"
+  },
+  {
+    "category": "Nature",
+    "title": "Passeio De Barco",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_1280.jpg"
+  },
+  {
+    "category": "Apoocalipse",
+    "title": "Paciente Zero",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2020/03/31/14/04/covid-19-4987797_1280.jpg"
+  },
+  {
+    "category": "Cinema",
+    "title": "Cineminha Caseiro",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2020/04/20/18/10/cinema-5069314_1280.jpg"
+  },
+  {
+    "category": "Halloween",
+    "title": "Abóbora",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2017/03/05/13/30/hallowen-2118650_1280.jpg"
+  },
+  {
+    "category": "CyberPunk",
+    "title": "Universo CyberPunk",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014616_1280.jpg"
+  },
+  {
+    "category": "Geografia",
+    "title": "Cidade",
+    "image_url":
+        "https://cdn.pixabay.com/photo/2023/01/19/10/24/castle-7728772_1280.jpg"
+  }
+];
