7b04f555f72208ad7d8dec4f5788098343e10909
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-02-24 02:14:36 -0300

    feat: create failure for domain

---
+++ lib/core/failures.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String? message;
+
+  const Failure({this.message});
+
+  @override
+  List get props => [message];
+}
+
+class UnhandledFailure extends Failure {
+  const UnhandledFailure({super.message});
+}
